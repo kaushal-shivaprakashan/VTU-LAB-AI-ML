79d5845ccd71632aade7c1d34e6a32f110e8841c
Kaushal Shivaprakash <86502925+kaushal-shivaprakashan@users.noreply.github.com>
2023-01-05 14:13:37 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,49 +1,40 @@
-import numpy as np
 import pandas as pd
-from scipy.stats import entropy
 from pprint import pprint
+from sklearn.feature_selection import mutual_info_classif
+from collections import Counter
 
-dataset = pd.read_csv("4.csv").iloc[:]
-features = dataset.columns[:-1].tolist()
-target = 'Target'
-def get_weighted_entropy(data, feature):
-    uf = data[target].value_counts(normalize = True)
-    weighted_entropy = 0
-    for i in range(len(uf)):
-        table = data.where(data[feature] == uf.index[i]).dropna()
-        table_entropy = entropy(table[target].value_counts())
+def id3(df, target_attribute, attribute_names, default_class=None):
+    cnt=Counter(x for x in df[target_attribute])
+    if len(cnt)==1:
+        return next(iter(cnt))
+    
+    elif df.empty or (not attribute_names):
+         return default_class
 
-        weighted_entropy += uf.values[i] * table_entropy
-    return weighted_entropy
-def ID3(data, features, most_freq_target_value):
-    
-    utv = data[target].value_counts()  
-    
-    if len(utv) <= 1: return utv.index[0]
-    elif len(data) == 0: return d_mftv
-    elif len(features) == 0: return most_freq_target_value
-    
     else:
-
-        #calculate information gain
-        ig = []
-        for feature in features:
-            total_entropy = entropy(data[feature].value_counts())
-            weighted_entropy = get_weighted_entropy(data, feature)
-            ig.append(total_entropy - weighted_entropy)
+        gainz = mutual_info_classif(df[attribute_names],df[target_attribute],discrete_features=True)
+        index_of_max=gainz.tolist().index(max(gainz))
+        best_attr=attribute_names[index_of_max]
+        tree={best_attr:{}}
+        remaining_attribute_names=[i for i in attribute_names if i!=best_attr]
         
-        #create tree
-        best_feature = features[np.argmax(ig)]
-        tree = {best_feature:{}}
-        features.remove(best_feature)
+        for attr_val, data_subset in df.groupby(best_attr):
+            subtree=id3(data_subset, target_attribute, remaining_attribute_names,default_class)
+            tree[best_attr][attr_val]=subtree
         
-        #create sub_features_table_and_sub_tree
-        sub_features = np.unique(data[best_feature])
-        for sf in sub_features:
-            sub_table = data.where(data[best_feature] == sf).dropna()
-            sub_tree = ID3(sub_table, features, utv.index[0])
-            tree[best_feature][sf] = sub_tree
-            
         return tree
-d_mftv = dataset[target].value_counts().index[0]
-pprint(ID3(dataset, features, d_mftv))
+    
+df=pd.read_csv("dataset46.csv")
+
+attribute_names=df.columns.tolist()
+print("List of attribut name")
+
+attribute_names.remove("PlayTennis")
+
+for colname in df.select_dtypes("object"):
+    df[colname], _ = df[colname].factorize()   
+print(df)
+
+tree= id3(df,"PlayTennis", attribute_names)
+print("The tree structure")
+pprint(tree)
